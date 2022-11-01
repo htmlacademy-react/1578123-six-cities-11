fef0d7bebb85f0c0a7d9dc69b10d30eb931f855b
Anton Vakhania <antonvakhania@mail.ru>
2022-11-01 16:45:13 +0300

    Заменил enum для Settings на литерал объекта

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
-enum Settings {
-  CardsCount = 5
-}
+const Settings = {
+  CardsCount: 5
+} as const;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
