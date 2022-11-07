4daa485092f5b7ecda950c4fd5058ef5c41a76af
Anton Vakhania <vakhania@opvf.ru>
2022-11-07 13:06:59 +0300

    Создаёт компонент PrivateRoute

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+
 import MainPage from '../../pages/main/main-page';
+import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import Room from '../../pages/room/room';
+import PageNotFound from '../../pages/page_404/page-404';
+
 import { HelmetProvider } from 'react-helmet-async';
 
 type AppScreenProps = {
