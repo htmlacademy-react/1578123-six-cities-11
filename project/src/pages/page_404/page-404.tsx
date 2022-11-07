6d711aad445122d2a8c9f589b9aaa2847eb1ae8b
Anton Vakhania <vakhania@opvf.ru>
2022-11-07 12:33:53 +0300

    Создаёт компонент страницы 404

--- project/src/pages/page_404/page-404.tsx
+++ project/src/pages/page_404/page-404.tsx
@@ -1,7 +1,28 @@
-export default function PageNotFound (): JSX.Element {
-    return (
-        <div>
-            
+import { Helmet } from "react-helmet-async";
+import { Fragment } from "react";
+
+import Logo from "../../components/logo/logo";
+import styles from "./page-404.module.css";
+
+export default function PageNotFound(): JSX.Element {
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Six cities|Not found</title>
+      </Helmet>
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Logo />
+            </div>
+          </div>
         </div>
-    );
+      </header>
+      <section className={styles.container}>
+        <h1 className={styles.error}>404 - Page not found</h1>
+        {/* Link */}
+      </section>
+    </Fragment>
+  );
 }
