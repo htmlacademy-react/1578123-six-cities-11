b38963c5379ec9c28e30daa19804fddc50c47cd0
Anton Vakhania <antonvakhania@mail.ru>
2022-11-14 12:59:04 +0300

    Добавляет моки для offers и reviews

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+import { offers } from './mocks/offers';
+import { reviews } from './mocks/reviews';
+ 
 const Settings = {
   CardsCount: 5
 } as const;
--- project/src/mocks/reviews.ts
+++
@@ -1,9 +0,0 @@
-import { Host } from './offers';
-
-export type Review = {
-    id: number;
-    rating: number;
-    comment: string;
-    author: Host;
-    date: string;
-};
