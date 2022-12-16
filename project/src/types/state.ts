a36672bd181a0baaa19aa665fd092a6a931b8104
Anton Vakhania <vakhania@opvf.ru>
2022-12-16 20:36:52 +0300

    Добавляет comments.test.ts

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -1,4 +1,12 @@
+import { FetchStatus } from '../const';
 import { store } from '../store';
+import { Review } from './reviews';
+
+export type Comments = {
+  comments: Review[];
+  fetchStatus: FetchStatus;
+  postStatus: FetchStatus;
+};
 
 export type State = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
